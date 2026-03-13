73d7d0f899e1eb7570ef5b8392c9e79690e520a3
DominicDev <s0l0estudi4r@gmail.com>
2026-03-13 17:50:51 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RUDE';
+export let nameSongs = [
+	"ETERNAL YOUTH", /*1*/
+	"FOREVER LOTUS", /*2*/
+	"FLUTTER", /*3*/
+	"ETERNAL YOUTH VIP", /*4*/
+	"GEMINI", /*5*/
+	"JASMIN", /*6*/
+	"BLOSSOM", /*7*/
+	"22 ROSES", /*8*/
+	"ANGEL'S CRY", /*9*/
+	"AWAKE", /*10*/
+	"WANDERER", /*11*/
+	"FOR YOU ALL.", /*12*/
+	"REFLECTION", /*13*/
+	"APEX", /*14*/
+	"STARDUST", /*15*/
+	"SEA OF ANGELS", /*16*/
+	"DOJO", /*17*/
+	"GYPSY", /*18*/
+	"SPIRIT", /*19*/
+	"SIREN", /*20*/
+]
